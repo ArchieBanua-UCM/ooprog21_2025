d1a60300e1c0d0f5ecf3f79e0ed04e28ca45cdcb
ArchieBanua-UCM <banuaarchie83@gmail.com>
2025-08-30 18:59:52 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,19 @@
+public class FixDebugOne {
+    /*This program displays greeting */
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+    }
+}
+
+/*
+List down the spotted bugs below:
+
+e.greeting
+
+-// wrong syntax for access modifier "publlic" instead of "public"
+
+Bugs:
+1.(missing '[]' in the function)
+2.(missing ';' in display)
+3.(missing close barracks on the public class)
+ */
