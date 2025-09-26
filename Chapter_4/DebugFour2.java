1341c3d8c497718a956f620a154a1dd604e613fe
ArchieBanua-UCM <banuaarchie83@gmail.com>
2025-09-26 12:18:48 +0800

    Add files via upload

---
+++ Chapter_4/DebugFour2.java
@@ -0,0 +1,39 @@
+// This class uses a DebugPen class
+// to instantiate two Pen objects
+// One with default values
+// and the other with user-supplied values
+
+import java.util.Scanner;
+
+public class DebugFour2
+{
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      String color;
+      String point;
+      DebugPen pen1;
+      DebugPen pen2;
+
+      System.out.print("Enter a color ink for the pen >> ");
+      color = input.nextLine();
+
+      System.out.print("Enter a point size - fine, medium, or thick >> ");
+      point = input.nextLine();
+
+      pen1 = new DebugPen(); // default values
+      pen2 = new DebugPen(color, point); // user values
+
+      System.out.println("\nDefault value pen:");
+      display(pen1);
+
+      System.out.println("\nUser value pen:");
+      display(pen2);
+   }
+
+   public static void display(DebugPen p)
+   {
+      System.out.println("   The pen has ink color " + p.getColor());
+      System.out.println("      and a " + p.getPoint() + " point.");
+   }
+}
--- Chapter_4/DebugPen.java
+++ Chapter_4/DebugPen.java
@@ -3,27 +3,23 @@ public class DebugPen
    private String color;
    private String point;
 
-   // Default constructor
    public DebugPen()
    {
       color = "black";
       point = "fine";
    }
 
-   // Parameterized constructor with proper assignments
    public DebugPen(String color, String point)
    {
       this.color = color;
       this.point = point;
    }
 
-   // Getter for color
    public String getColor()
    {
       return color;
    }
 
-   // Getter for point
    public String getPoint()
    {
       return point;
